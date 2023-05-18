f12b4072dc32edd393c0a1efbed31a6a20e24e2d
John <johnkaranjairungu@gmail.com>
2023-05-18 14:38:33 +0300

    added setters and getters to code

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
+    # Getter method for @name
+    def name
+      @name
+    end
+    # Setter method for @name
+    def name=(name)
+      @name = name
+    end
+    # Getter method for @breed
+    def breed
+      @breed
+    end
+    # setter method for @breed
+    def breed=(breed)
+      @breed = breed
+    end
 end
+    
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    # Getter method for @name
+    def name
+        @name
+      end
+      # Setter method for @name
+      def name=(name)
+        @name = name
+      end
 
+      # Getter method for @job
+      def job
+        @job
+      end
+      # setter method for @job
+      def job=(job)
+        @job = job
+      end
 end
